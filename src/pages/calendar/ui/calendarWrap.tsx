255058d21349c38c9fe2a2bd7749ecf0c0397d02
Cameist <0kay0802@gmail.com>
2025-01-16 16:36:46 +0900

    feat: 캘린더 컴포넌트 추가

--- src/pages/calendar/ui/calendarWrap.tsx
+++ src/pages/calendar/ui/calendarWrap.tsx
@@ -47,10 +47,18 @@ type Events = {
 type CalendarWrapperProps = {
   selectedDate: Date | null;
   onDateChange: (date: Date | null) => void;
+  currentMonth: Date;
+  onMonthChange: (month: Date) => void;
   events: Events; // 일정 데이터 추가
 };
 
-const CalendarWrapper = ({ selectedDate, onDateChange, events }: CalendarWrapperProps) => {
+const CalendarWrapper = ({
+  selectedDate,
+  onDateChange,
+  currentMonth,
+  onMonthChange,
+  events,
+}: CalendarWrapperProps) => {
   // 특정 날짜에 일정이 있는지 확인하는 함수
   const dateHasEvent = (date: Date) => {
     const dateKey = date.toDateString();
@@ -69,6 +77,11 @@ const CalendarWrapper = ({ selectedDate, onDateChange, events }: CalendarWrapper
             onDateChange(null);
           }
         }}
+        onActiveStartDateChange={({ activeStartDate }) => {
+          if (activeStartDate) {
+            onMonthChange(activeStartDate); // 월 변경 핸들러 호출
+          }
+        }}
         value={selectedDate}
         className="react-calendar-custom"
         tileClassName={({ date, view }) =>
@@ -79,9 +92,12 @@ const CalendarWrapper = ({ selectedDate, onDateChange, events }: CalendarWrapper
         prev2Label={null}
         next2Label={null}
         showNeighboringMonth={false}
+        activeStartDate={currentMonth} // 현재 월 상태와 동기화
       />
     </Box>
   );
 };
 
 export default CalendarWrapper;
+
+

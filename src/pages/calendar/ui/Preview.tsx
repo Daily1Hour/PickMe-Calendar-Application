5096bbf7c52835d306bb658f3bc407549d60f79d
Cameist <0kay0802@gmail.com>
2025-02-25 16:31:49 +0900

    feat: 프리뷰 데이터 연결

--- src/pages/calendar/ui/Preview.tsx
+++ src/pages/calendar/ui/Preview.tsx
@@ -8,18 +8,26 @@ type PreviewProps = {
 };
 
 const Preview = ({ events, selectedDate }: PreviewProps) => {
+  console.log("Preview events:", events);
   return (
-    <Box mt={6}>
+    <Box mt={6} w="20vw">
       <Text fontSize="lg">
         {selectedDate ? selectedDate.toDateString() : "날짜를 선택해주세요."}의
         일정:
       </Text>
       {events.map((event, index) => (
-        <Box key={index} p={4} borderWidth="1px" borderRadius="lg" my={2}>
+        <Box
+          key={index}
+          p={4}
+          borderWidth="1px"
+          borderRadius="lg"
+          my={2}
+          maxW="20vw"
+        >
           <HStack>
             <GrStatusGoodSmall color="teal" />
             <Text>
-              회사: {event.name} | 장소: {event.location}
+              회사: {event.company.name} | 장소: {event.company.location}
             </Text>
           </HStack>
         </Box>
